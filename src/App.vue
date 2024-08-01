24b5897951661bbfe0242f22949e91fe5c0dacf1
柒玖 <qijiuhcl@qq.com>
2024-08-01 11:58:45 +0800

    初始化仓库

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<script setup>
+import {} from 'vue'
+import demo from './views/demo.vue'
+</script>
+
+<template>
+  <div class="">
+    <demo></demo>
+  </div>
+</template>
+
+<style lang="less" scoped></style>
---
+++ src/views/demo.vue
@@ -0,0 +1,9 @@
+<script setup>
+import {} from "vue"
+</script>
+
+<template>
+  <div class="">Book sales system</div>
+</template>
+
+<style lang="less" scoped></style>
