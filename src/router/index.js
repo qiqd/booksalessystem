3b82bb04987d40f20f202e561dad139467763c9a
柒玖 <qijiuhcl@qq.com>
2024-10-13 19:39:47 +0800

    优化项目

--- src/router/index.js
+++ src/router/index.js
@@ -2,97 +2,96 @@ import { createRouter, createWebHistory } from 'vue-router'
 import { useCounterStore } from '@/stores/counter'
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
+  // base: 'yes',
   routes: [
     {
       path: '/',
       name: 'login',
-      component: () => import("@/views/AdminLogin.vue")
+      component: () => import('@/views/AdminLogin.vue')
     },
     {
       path: '/layout',
       name: 'layout',
       redirect: '/index',
-      component: () => import("@/views/LayoutPage.vue"),
+      component: () => import('@/views/LayoutPage.vue'),
       children: [
         {
           path: '/index',
           name: 'index',
-          component: () => import("@/views/BookSaleInfo.vue")
+          component: () => import('@/views/BookSaleInfo.vue')
         },
         {
           path: '/staff',
           name: 'staff',
-          component: () => import("@/views/StaffManager.vue")
+          component: () => import('@/views/StaffManager.vue')
         },
         {
           path: '/StaffIncrease',
           name: 'StaffIncrease',
-          component: () => import("@/views/StaffIncrease.vue")
+          component: () => import('@/views/StaffIncrease.vue')
         },
         {
           path: '/vip',
           name: 'vip',
-          component: () => import("@/views/VipUser.vue")
+          component: () => import('@/views/VipUser.vue')
         },
         {
           path: '/manager',
           name: 'manager',
-          component: () => import("@/views/AdminManager.vue")
+          component: () => import('@/views/AdminManager.vue')
         },
         {
           path: '/admin',
           name: 'admin',
-          component: () => import("@/views/AdminInfo.vue")
+          component: () => import('@/views/AdminInfo.vue')
         },
         {
           path: '/password',
           name: 'password',
-          component: () => import("@/views/AdminPassword.vue")
+          component: () => import('@/views/AdminPassword.vue')
         },
         {
           path: '/orders',
           name: 'orders',
-          component: () => import("@/views/Orders.vue")
+          component: () => import('@/views/Orders.vue')
         },
         {
           path: '/books',
           name: 'books',
-          component: () => import("@/views/BooksInfo.vue")
+          component: () => import('@/views/BooksInfo.vue')
         },
         {
           path: '/booktype',
           name: 'booktype',
-          component: () => import("@/views/BookTypeInfo.vue")
+          component: () => import('@/views/BookTypeInfo.vue')
         },
         {
           path: '/shelf',
           name: 'shelf',
-          component: () => import("@/views/ShelfInfo.vue")
+          component: () => import('@/views/ShelfInfo.vue')
         },
         {
           path: '/AdminIncrease',
           name: 'AdminIncrease',
-          component: () => import("@/views/AdminIncrease.vue")
+          component: () => import('@/views/AdminIncrease.vue')
         },
         {
           path: '/VipIncrease',
           name: 'VipIncrease',
-          component: () => import("@/views/VipIncrease.vue")
+          component: () => import('@/views/VipIncrease.vue')
         },
         {
           path: '/TypeIncrease',
           name: 'TypeIncrease',
-          component: () => import("@/views/BookTypeIncrease.vue")
-        }, {
+          component: () => import('@/views/BookTypeIncrease.vue')
+        },
+        {
           path: '/BookIncrease',
           name: 'BookIncrease',
-          component: () => import("@/views/BookIncrease.vue")
-        },
-
+          component: () => import('@/views/BookIncrease.vue')
+        }
       ]
-    },
-
-
+    }
   ]
 })
 router.beforeEach((to) => {
