42eea9614a3e073584a59d9d5be741f8b9846687
Oguri Cap <xuyiji092854@126.com>
2024-10-13 23:49:25 +0800

    修复bug

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,9 @@ import Components from 'unplugin-vue-components/vite'
 import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-
+// eslint-disable-next-line no-control-regex
+const INVALID_CHAR_REGEX = /[\x00-\x1F\x7F<>*#"{}|^[\]`;?:&=+$,]/g
+const DRIVE_LETTER_REGEX = /^[a-z]:/i
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/booksalessystem',
@@ -17,6 +19,18 @@ export default defineConfig({
       resolvers: [ElementPlusResolver()]
     })
   ],
+  build: {
+    rollupOptions: {
+      output: {
+        // https://github.com/rollup/rollup/blob/master/src/utils/sanitizeFileName.ts
+        sanitizeFileName(fileName) {
+          const match = DRIVE_LETTER_REGEX.exec(fileName)
+          const driveLetter = match ? match[0] : ''
+          return driveLetter + fileName.slice(driveLetter.length).replace(INVALID_CHAR_REGEX, '')
+        }
+      }
+    }
+  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
